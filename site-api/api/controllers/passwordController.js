328e291eaf842dccd8fab56fa8bd2995164753b2
maxgardiner <mgardine@alumni.uoguelph.ca>
2018-01-25 14:58:15 -0500

    Still trying to get node api to connect to angular site on ec2 instance

--- site-api/api/controllers/passwordController.js
+++ site-api/api/controllers/passwordController.js
@@ -1,13 +1,18 @@
 'use strict';
 const HARDCODED_PASSWORD = process.env.RESUME_PASSWORD || 'ANYPASSWORD';
+const ENVIRONMENT = process.env.ENV_TYPE || 'dev';
 
 //Checks given request against the password for the full resume
 exports.check_passwords = function(req, res) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://18.216.135.73:8081'); //for dev only
-    res.setHeader('Access-Control-Allow-Origin', 'http://18.216.135.73:80'); //for prod http only
-    res.setHeader('Access-Control-Allow-Origin', 'http://18.216.135.73:443'); //for prod https only
+    if (ENVIRONMENT == 'prod'){
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', 'http://18.216.135.73:80'); //for prod http only
+        res.setHeader('Access-Control-Allow-Origin', 'http://18.216.135.73:443'); //for prod https only
+    }
+    else{
+        res.setHeader('Access-Control-Allow-Origin', 'http://18.216.135.73:8081'); //for dev only
+    }
 
     //if there is an error
     if ( req.query.attempted_password == null){
