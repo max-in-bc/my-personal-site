cd63a3ada2c457acf14a1d6543c60727d2fba461
maxgardiner <mgardine@alumni.uoguelph.ca>
2018-01-25 14:55:46 -0500

    Added elastic IP from AMAZON http://18.216.135.73/

--- site-api/api/controllers/passwordController.js
+++ site-api/api/controllers/passwordController.js
@@ -4,6 +4,11 @@ const HARDCODED_PASSWORD = process.env.RESUME_PASSWORD || 'ANYPASSWORD';
 //Checks given request against the password for the full resume
 exports.check_passwords = function(req, res) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://18.216.135.73:8081'); //for dev only
+    res.setHeader('Access-Control-Allow-Origin', 'http://18.216.135.73:80'); //for prod http only
+    res.setHeader('Access-Control-Allow-Origin', 'http://18.216.135.73:443'); //for prod https only
+
     //if there is an error
     if ( req.query.attempted_password == null){
         res.json({results: -1});
