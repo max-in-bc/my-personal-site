4293ea553e362e9c6d3a113bf95176d80204ad03
max-in-to <mgardine@alumni.uoguelph.ca>
2018-05-07 23:40:48 -0400

    move api to separate repo

--- site-api/api/routes/resumeRoutes.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-module.exports = function(app) {
-    var resumeCtrl = require('../controllers/resumeController');
-
-    app.route('/fullresume')
-        .get(resumeCtrl.get_full_resume);
-};
